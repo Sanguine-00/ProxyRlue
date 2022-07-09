d8f79c4175603b2c28c9fd8d4a446dde7809f18e
Sanguine-00 <1216160345@qq.com>
2022-07-09 22:10:28 +0800

    Create symbolab.js

---
+++ symbolab.js
@@ -0,0 +1,37 @@
+/*
+酷我音乐 解锁会员试听及部分功能
+
+***************************
+QuantumultX:
+
+# Symbolab 解锁订阅
+
+[rewrite_local]
+^https?:\/\/scibug\.com\/appleSubscriptionValidate$ url script-response-body script-path=https://raw.githubusercontent.com/Sanguine-00/ProxyRlue/main/symbolab.js
+[mitm]
+hostname = scibug.com
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https?:\/\/scibug\.com\/appleSubscriptionValidate requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Sanguine-00/ProxyRlue/main/symbolab.js
+
+[MITM]
+hostname = scibug.com
+
+**************************/
+
+
+
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.data["valid"] = true;
+obj.data["hasUserConsumedAppleFreeTrial"] = false;
+obj.data["isCurrentlyInFreeTrial"] = false;
+obj.data["newlyAssociated"] = false;
+
+body = JSON.stringify(obj);
+$done({body});
