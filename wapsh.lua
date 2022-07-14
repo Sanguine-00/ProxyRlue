cde903368935c390baac811677e7470ddba0add6
Sanguine-00 <1216160345@qq.com>
2022-07-14 10:20:31 +0800

    Update wapsh.lua

--- wapsh.lua
+++ wapsh.lua
@@ -44,8 +44,8 @@ function wa_lua_on_handshake_cb(ctx)
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT ' .. host .. ':' .. port .. '@wapsd.189.cn:80 HTTP/1.1\r\n' ..
-                    'Host: wapsd.189.cn:80\r\n' ..
+        local res = 'CONNECT ' .. host .. ':' .. port .. '@a.189.cn:80 HTTP/1.1\r\n' ..
+                    'Host: a.189.cn:80\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: YTY0Nzlk\r\n' ..
                     'User-Agent: baiduboxapp\r\n\r\n'
