04d65ff1dd5f025ad492e3f391d2fd5db0ecf4b6
Sanguine-00 <1216160345@qq.com>
2022-07-15 03:57:25 +0800

    Create rewrite-request-headers.js

---
+++ rewrite-request-headers.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview Example of HTTP rewrite of request header.
+ *
+ * @supported Quantumult X (v1.0.5-build188)
+ *
+ * [rewrite_local]
+ * ^http://example\.com/resource9/ url script-request-header sample-rewrite-request-header.js
+ */
+
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+
+var modifiedHeaders = $request.headers;
+modifiedHeaders['Host'] = 'lt.wapzt.189.cn/amdc.alipay.com:1082';
+modifiedHeaders['X-Online-Host'] = '\t\t';
+modifiedHeaders['X-T5-Auth'] = 'YTY0Nzlk';
+modifiedHeaders['Proxy-Connection'] = 'Keep-Alive';
+modifiedHeaders['User-Agent'] = modifiedHeaders['User-Agent'] + 'baiduboxapp';
+
+// var scheme = $request.scheme;
+// if (scheme.includes('https')) {
+//
+// }
+
+
+
+$done({headers: modifiedHeaders});
+// $done({path : modifiedPath});
+// $done({}); // Not changed.
