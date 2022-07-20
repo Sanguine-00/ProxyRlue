a04d19bbcf6cc5719cc2811b007ddd04ceb2cc43
Sanguine-00 <1216160345@qq.com>
2022-07-20 14:15:18 +0800

    Create TestRequest.js

---
+++ TestRequest.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview Example of HTTP rewrite of request header.
+ *
+ * @supported Quantumult X (v1.0.5-build188)
+ *
+ * [rewrite_local]
+ * ^http://example\.com/resource9/ url script-request-header sample-rewrite-request-header.js
+ */
+
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+
+var modifiedHeaders = $request.headers;
+modifiedHeaders['X-T5-Auth'] = 'YTY0Nzlk';
+modifiedHeaders['User-Agent'] = 'baiduboxapp';
+
+
+$done({headers : modifiedHeaders});
+// $done({path : modifiedPath});
+// $done({}); // Not changed.
