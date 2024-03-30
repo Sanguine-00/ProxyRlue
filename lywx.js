9ddb98e25b8158362acc99fd15c71e0eca06777e
Quintin <18140259+Sanguine-00@users.noreply.github.com>
2024-03-30 20:59:40 +0800

    Create lywx.js

---
+++ lywx.js
@@ -0,0 +1,19 @@
+/*************************************
+项目名称：乐游无锡
+下载地址：
+更新日期：2024-02-27
+脚本作者：
+使用声明：
+**************************************
+
+[rewrite_local]
+^https:\/\/www\.taihumingzhuwx\.com\/api\/trip-scard-core\/bus\/card\/products url script-response-body https://raw.githubusercontent.com/Sanguine-00/ProxyRlue/main/lywx.js
+[mitm]
+hostname = www.taihumingzhuwx.com
+*************************************/
+var bodyRes = JSON.parse($response.body);
+bodyRes.result[0].isOpen = true;
+bodyRes.result[0].cardNo = '6032020170589029';
+bodyRes.result[0].cardpCode = '00001';
+
+$done({body : JSON.stringify(bodyRes)});
